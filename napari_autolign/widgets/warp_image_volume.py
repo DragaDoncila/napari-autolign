ef71b9b5a1eef8b14e952a26c954192d1a5fdb8b
krentzd <daniel.krentzel@crick.ac.uk>
2021-05-27 18:02:32 +0100

    Allow labels to be warped

--- napari_autolign/widgets/warp_image_volume.py
+++ napari_autolign/widgets/warp_image_volume.py
@@ -8,7 +8,7 @@ from probreg import bcpd
 import napari
 from magicgui import magic_factory, widgets
 from napari.types import PointsData, ImageData
-from napari.layers import Points
+from napari.layers import Points, Layer, Image, Labels
 from typing_extensions import Annotated
 import math
 from typing import Sequence
@@ -113,7 +113,7 @@ def _make_warp(from_points, to_points, x_vals, y_vals, z_vals):
     print('Computing pseudoinverse of L...')
     print(L.shape)
     # TODO: benchmark speed of numpy and scipy implementations of pinv
-    # TODO: if piecewise non-linear transform only compute pseudoinverse once! 
+    # TODO: if piecewise non-linear transform only compute pseudoinverse once!
     L_pseudo_inverse = np.linalg.pinv(L) # L increases with number of control points!
     print('Done!')
     coeffs = np.dot(L_pseudo_inverse, V)
@@ -161,8 +161,8 @@ def on_init(widget):
 @magic_factory(widget_init=on_init, layout='vertical', call_button="Warp")
 def make_image_warping(
     viewer: "napari.viewer.Viewer",
-    moving_image: ImageData,
-    fixed_image: ImageData,
+    moving_image: Layer,
+    fixed_image: Layer,
     transform_type: Annotated[str, {"choices": ["Rigid", "Affine", "Deformable"]}],
     moving_points: Points,
     transformed_points: Points,
@@ -170,6 +170,9 @@ def make_image_warping(
     approximate_grid: Annotated[int, {"min": 1, "max": 10, "step": 1}]=1,
     sub_division_factor: Annotated[int, {"min": 1, "max": 10, "step": 1}]=1
 ):
+    assert isinstance(moving_image, Image) or isinstance(moving_image, Labels), 'moving_image must be Image or Labels'
+    assert isinstance(fixed_image, Image) or isinstance(fixed_image, Labels), 'fixed_image must be Image or Labels'
+
     pbar = widgets.ProgressBar()
     pbar.range = (0, 0)  # unknown duration
     make_image_warping.insert(0, pbar)  # add progress bar to the top of widget
@@ -177,7 +180,10 @@ def make_image_warping(
     # this function will be called after we return
     def _add_data(return_value, self=make_image_warping):
         data, kwargs = return_value
-        viewer.add_image(data, **kwargs)
+        if isinstance(moving_image, Image):
+            viewer.add_image(data, **kwargs)
+        elif isinstance(moving_image, Labels):
+            viewer.add_labels(data, **kwargs)
         self.pop(0).hide()  # remove the progress bar
 
     @thread_worker(connect={"returned": _add_data})
@@ -232,8 +238,8 @@ def make_image_warping(
         return (warped_image, kwargs)
 
     if transform_type == 'Deformable':
-        _warp_image_volume(moving_image=moving_image,
-                           fixed_image=fixed_image,
+        _warp_image_volume(moving_image=moving_image.data,
+                           fixed_image=fixed_image.data,
                            moving_points=moving_points.data,
                            transformed_points=transformed_points.data,
                            interpolation_order=interpolation_order,
@@ -243,9 +249,12 @@ def make_image_warping(
     elif transform_type == 'Affine' or transform_type == 'Rigid':
         affine_matrix = transformed_points.affine.affine_matrix
         print(affine_matrix)
-        img_wrp = _warp_image_volume_affine(image=moving_image,
+        img_wrp = _warp_image_volume_affine(image=moving_image.data,
                                             matrix=affine_matrix,
-                                            output_shape=fixed_image.shape,
+                                            output_shape=fixed_image.data.shape,
                                             interpolation_order=interpolation_order)
-        viewer.add_image(img_wrp)
+        if isinstance(moving_image, Image):
+            viewer.add_image(img_wrp)
+        elif isinstance(moving_image, Labels):
+            viewer.add_labels(img_wrp)
         make_image_warping.pop(0).hide()
